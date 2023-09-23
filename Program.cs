f91332404a6ddfbb74d796ad0ef244c73e41159a
Кирилл Парыгин <parygin.ks@gmail.com>
2023-09-23 16:45:41 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Задача_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write number 1: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Write number 2: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            if (n < m)
+            {
+                for (int i = n; i <= m; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+            else 
+            {
+                int tmp = m;
+                m = n;
+                n=tmp;
+                for (int i = n; i <= m; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
